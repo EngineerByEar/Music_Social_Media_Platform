44c2c398edb2070553b77266a0cea9fd57a1e369
Mike37-1 <mt241063@fhstp.ac.at>
2026-01-22 10:08:15 +0100

    test setup complete. Started on API Documentation

--- MusicAPI/dist/index.js
+++ MusicAPI/dist/index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
+import YAML from "yaml";
 import cors from "cors";
 const PORT = process.env.PORT || 3000;
 const app = express();
+//Load API Documentation
+const openApiPath = "./openapi.yml";
+const file = fs.readFileSync(openApiPath, "utf8");
+const swaggerDocument = YAML.parse(file);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(cors());
 app.get("/", (req, res) => {
     res.send({ "message": "Hello Express from TS" });
